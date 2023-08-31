19b64a22b36124aa39dfd4bd6919701bd7987d60
ViniciusMV13 <viniciuscordeiro789@gmail.com>
2023-08-31 18:15:25 -0300

    Intro do site finalizada

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,7 @@
-import {HomeContainer, InfoContainer, Iten1, Itens, Iten2, Iten3, Iten4,} from './styles'
+import {HomeContainer, InfoContainer, Item1, Item2, Item3, Item4, Itens} from './styles'
 import Background from "./../../assets/Background.svg"
+import CoffeeImg from "./../../assets/Coffee.svg"
+import { Timer, ShoppingCart, Package, Coffee } from 'phosphor-react'
 
 export function Home(){
   return(
@@ -8,12 +10,15 @@ export function Home(){
       <h1>Encontre o café perfeito para qualquer hora do dia</h1>
       <h3>Com o Coffee Delivery você recebe seu café onde estiver, a qualquer hora</h3>
       <Itens>
-        <Iten1>Compra simples e segura</Iten1>
-        <Iten2>Entrega rápida e rastreada</Iten2>
-        <Iten3>Embalagem mantém o café intacto</Iten3>
-        <Iten4>O café chega fresquinho até você</Iten4>
+        <Item1><ShoppingCart size={32} weight="fill"/>Compra simples e segura</Item1>
+        <Item2><Package size={32} weight="fill"/>Embalagem mantém o café intacto</Item2>
+        <Item3><Timer size={32} weight="fill"/>Entrega rápida e rastreada</Item3>
+        <Item4><Coffee size={32} weight="fill"/>O café chega fresquinho até você</Item4>
       </Itens>
       </InfoContainer>
+      <span>
+        <img src={CoffeeImg}/>
+      </span>
     </HomeContainer>
   )
 }
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -7,26 +7,69 @@ height: 34rem;
 background-repeat: no-repeat;
 background-size: 100%;
 padding: 5.75rem 10rem;
+display: flex;
+justify-content: space-between;
+
+img{
+  height: 22.5rem;
+
+}
 ` 
 export const InfoContainer = styled.div`
+width: 36.75rem;
 h1{
   font-family: 'Baloo 2';
+  font-size: 3rem;
+  font-weight: 800;
+  margin-bottom: 1rem;
+  color: ${(props) => props.theme['base-title']};
+}
+h3{
+  font-size: 1.25rem;
+  font-weight: 400;
+  color: ${(props) => props.theme['base-subtitle']};
 }
 `
 export const Itens = styled.div`
 display: flex;
 flex-wrap: wrap;
-width: 36.75rem;
+margin-top: 4.125rem;
+
 `
-export const Iten1 = styled.div`
+
+const Item = styled.div`
+color: ${(props) => props.theme['base-text']};
+margin-bottom: 1.250rem;
 width: 50%;
+font-weight: 400;
+font-size: 1rem;
+display: flex;
+justify-content: left;
+align-items: center;
+gap: 0.75rem;
+svg {
+  padding: 0.5rem;
+  border-radius: 999px;
+  color: white;
+}
 `
-export const Iten2 = styled.div`
-width: 50%;
+export const Item1 = styled(Item)`
+svg{
+background: ${(props) => props.theme['yellow-dark']};;
+}
 `
-export const Iten3 = styled.div`
-width: 50%;
+export const Item2 = styled(Item)`
+svg {
+background: ${(props) => props.theme['base-text']};;
+}
 `
-export const Iten4 = styled.div`
-width: 50%;
+export const Item3 = styled(Item)`
+svg {
+background: ${(props) => props.theme['yellow']};;
+}
+`
+export const Item4 = styled(Item)`
+svg {
+background: ${(props) => props.theme['purple']};;
+}
 `
